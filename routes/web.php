e7871432fab42a2ee641f55f1391b289b705f273
tadeyinka <tohbyyy@gmail.com>
2021-08-04 07:58:11 +0700

    layout setup
    Setup admin files and layout

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', 'AdminController@index');
+});
